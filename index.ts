756cf8fabab70996c0841a4422780edd07e702bc
May Tusek <tusek.may@gmail.com>
2024-02-12 00:58:10 -0800

    Adds basic editor.

--- index.ts
+++ index.ts
@@ -20,7 +20,13 @@ if (target === null) {
   throw new Error("Unable to find render target");
 }
 
-const width = window.innerWidth;
+const editor = document.querySelector<HTMLPreElement>("#editor");
+
+if (editor === null) {
+  throw new Error("Unable to find editor");
+}
+
+const width = window.innerWidth / 2;
 const height = window.innerHeight;
 
 const scene = new Scene();
@@ -45,11 +51,11 @@ const geometry = new PlaneGeometry(2, 2);
 const rectangle = new BoxGeometry(1, 1, 1);
 
 const material = new ShaderMaterial({
-  fragmentShader: fragment,
+  fragmentShader: document.querySelector("#editor")?.textContent ?? "",
   uniforms,
 });
 
-const cube = new Mesh(geometry, material);
+let cube = new Mesh(geometry, material);
 
 scene.add(cube);
 
@@ -62,8 +68,8 @@ const render = () => {
 
 const onWindowResize = () => {
   renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  uniforms.u_resolution.value.x = window.innerWidth;
+  renderer.setSize(window.innerWidth / 2, window.innerHeight);
+  uniforms.u_resolution.value.x = window.innerWidth / 2;
   uniforms.u_resolution.value.y = window.outerHeight;
 };
 
@@ -73,6 +79,12 @@ window.onmousemove = (e) => {
 };
 
 window.addEventListener("resize", onWindowResize, false);
+
+editor.addEventListener("input", (e) => {
+  material.fragmentShader = (e.target as HTMLPreElement).textContent!;
+  cube.material = material;
+  cube.material.needsUpdate = true;
+});
 
 onWindowResize();
 render();
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './index.ts',
   mode: 'development',
   devServer: {
-    watchFiles: ["./shaders/*"]
+    watchFiles: ["./shaders/*", "*.html"]
   },
   module: {
     rules: [
