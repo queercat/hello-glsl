aee1dea91c6b33842862ffe177760b608178b9ed
May Tusek <tusek.may@gmail.com>
2024-02-12 00:07:04 -0800

    Cube...

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { BoxGeometry, Mesh, PerspectiveCamera, Scene, ShaderMaterial, WebGLRenderer } from "three";
+import { BoxGeometry, Clock, Mesh, PerspectiveCamera, Scene, ShaderMaterial, Vector2, WebGLRenderer } from "three";
 
 import vertex from "/shaders/vertex.glsl";
 import fragment from "/shaders/fragment.glsl";
@@ -13,9 +13,9 @@ const width = window.innerWidth;
 const height = window.innerHeight;
 
 const scene = new Scene();
-const camera = new PerspectiveCamera(45, width / height, 0.1, 1);
+const camera = new PerspectiveCamera(45, width / height, 0.1, 100);
 
-camera.position.set(0, 0, 1);
+camera.position.set(0, 0, 5);
 camera.lookAt(0, 0, 0);
 
 const renderer = new WebGLRenderer();
@@ -23,12 +23,43 @@ renderer.setSize(width, height);
 
 target.appendChild(renderer.domElement);
 
+const clock = new Clock();
+
+const uniforms = {
+  u_time: { type: "f", value: 1.0 },
+  u_resolution: { type: "v2", value: new Vector2() },
+  u_mouse: { type: "v2", value: new Vector2() },
+};
+
 const rectangle = new BoxGeometry(1, 1, 1);
+
 const material = new ShaderMaterial({
-  vertexShader: vertex,
   fragmentShader: fragment,
+  uniforms,
 });
+
 const cube = new Mesh(rectangle, material);
 
 scene.add(cube);
-renderer.render(scene, camera);
+
+const render = () => {
+  renderer.render(scene, camera);
+  uniforms.u_time.value += clock.getDelta();
+  cube.rotateX(0.01);
+  cube.rotateY(0.01);
+  cube.rotateZ(0.01);
+  window.requestAnimationFrame(render);
+};
+
+const onWindowResize = () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  uniforms.u_resolution.value.x = renderer.domElement.width;
+  uniforms.u_resolution.value.y = renderer.domElement.height;
+};
+
+window.addEventListener("resize", () => {
+  onWindowResize();
+});
+
+onWindowResize();
+render();
