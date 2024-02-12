0b87caae7223053d091e2d667c13526882271a37
May Tusek <tusek.may@gmail.com>
2024-02-11 23:16:29 -0800

    Begins using three.

--- index.ts
+++ index.ts
@@ -1,3 +1,18 @@
-import * as three from "three";
+import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
 
-console.log(three)
+const target = document.querySelector("#renderContainer");
+
+if (target === null) {
+  throw new Error("Unable to find render target");
+}
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const scene = new Scene();
+const camera = new PerspectiveCamera(75, width / height, 0.1, 1000);
+
+const renderer = new WebGLRenderer();
+renderer.setSize(width, height);
+
+target.appendChild(renderer.domElement);
